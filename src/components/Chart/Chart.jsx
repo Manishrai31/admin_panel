8ed0c44ee9b5cf9b6c081ff3f4f6a52e23a0caab
Manish Rai <manishrai@ip-192-168-0-104.ap-south-1.compute.internal>
2022-10-05 23:40:54 +0530

    Add chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,10 +1,95 @@
 import React from "react";
 import "./chart.scss";
-const Chart=()=>{
-    return(
-        <div className="chart">
-            Charts
-        </div>
-    )
-}
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const Chart = () => {
+  const data = [
+    {
+      name: "March",
+      Withdrawl: "2002",
+      Credit : "1234"
+    },
+    {
+      name: "April",
+      Withdrawl: "1006",
+      Credit : "4353"
+
+    },
+    {
+      name: "May",
+      Withdrawl: "0987",
+      Credit : "2353"
+
+    },
+    {
+      name: "July",
+      Withdrawl: "0683",
+      Credit : "1234"
+
+    },
+    {
+      name: "August",
+      Withdrawl: "3456",
+      Credit : "1000"
+
+    },
+    {
+      name: "September",
+      Withdrawl: "1982",
+      Credit : "6532"
+
+    },
+  ];
+  return (
+    <div className="chart">
+      <div className="title">Last 6 Months Revenue</div>
+      <ResponsiveContainer width="100%" aspect={2 / 1}>
+        <AreaChart
+          width={730}
+          height={250}
+          data={data}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        >
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" stroke="gray"/>
+          {/* <YAxis /> */}
+          <CartesianGrid strokeDasharray="3 3" className="chartGrid"/>
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="Withdrawl"
+            stroke="#8884d8"
+            fillOpacity={1}
+            fill="url(#colorUv)"
+          />
+          <Area
+            type="monotone"
+            dataKey="Credit"
+            stroke="#82ca9d"
+            fillOpacity={1}
+            fill="url(#colorPv)"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
 export default Chart;
--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,9 +1,37 @@
 import React from "react";
 import './featured.scss'
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { CircularProgressbar } from "react-circular-progressbar";
+import 'react-circular-progressbar/dist/styles.css';
 const Featured = ()=>{
     return (
         <div className="featured">
-            Featured
+            <div className="top">
+                <h3 className="title">Wallet Transection</h3>
+                <MoreVertIcon fontSize="small"/>
+            </div>
+            <div className="bottom">
+                <div className="featuredChart">
+                    <CircularProgressbar value={60} text={"60%"}  strokeWidth={5}/>
+                </div>
+                
+                <p className="title">Total transection made today</p>
+                <p className="amount">$1000</p>
+                <p className="desc">
+                    Previous transection processing.
+                </p>
+
+                <div className="successRate">
+                    <div className="left">
+                        <p className="complete">Complete</p>
+                        <p className="amount">$600</p>
+                    </div>
+                    <div className="right">
+                        <p className="incomplete">Incomplete</p>
+                        <p className="amount">$400</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
